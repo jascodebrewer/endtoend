2b80752cf70f0cdda5b5b57dec5670f7918b3fd1
jascodebrewer <jasofficial1214@gmail.com>
2023-12-05 17:02:19 +0530

    updated code

--- src/DiamondPricePrediction/pipelines/training_pipeline.py
+++ src/DiamondPricePrediction/pipelines/training_pipeline.py
@@ -1,3 +1,5 @@
+from src.DiamondPricePrediction.components.data_transformation import DataTransformation
+from src.DiamondPricePrediction.components.model_trainer import ModelTrainer
 from src.DiamondPricePrediction.components.data_ingestion import DataIngestion
 
 import os
@@ -9,3 +11,9 @@ import pandas as pd
 obj=DataIngestion()
 
 train_data_path,test_data_path=obj.initiate_data_ingestion()
+
+data_transformation_obj=DataTransformation()
+train_arr, test_arr=data_transformation_obj.initialize_data_transformation(train_data_path, test_data_path)
+
+model_trainer_obj=ModelTrainer()
+model_trainer_obj.initate_model_training(train_arr, test_arr)
